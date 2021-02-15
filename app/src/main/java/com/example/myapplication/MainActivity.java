ffa0ad14749902e7d670e5fe064b10a8c5973dab
YvesImmer <yvesimmer@gmail.com>
2021-02-15 15:19:24 +0100

    button

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,14 +2,27 @@ package com.example.myapplication;
 
 
 import android.app.Activity;
-import android.content.Intent
-import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
-import com.example.myapplication.R
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-    class MainActivity extends Activity {
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import com.example.myapplication.R;
+    class MainActivity extends Activity implements View.OnClickListener {
+        @Override
+        protected void onCreate(@Nullable Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
+            Button btnNext = findViewById(R.id.button_next_activity);
+            btnNext.setOnClickListener(this);
+        }
 
-
-
+        @Override
+        public void onClick(View v) {
+            Intent intent = new Intent(this, SecondaryActivity.class);
+            startActivity(intent);
+        }
     }
-}
+
