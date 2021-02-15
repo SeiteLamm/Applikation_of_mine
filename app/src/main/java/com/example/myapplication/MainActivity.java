a97dd4e1814b29e2c071336d3a048774c6c8447c
Iram <inkonito67@gmail.com>
2021-02-15 15:07:48 +0100

    change to java

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.myapplication;
+
+
+import android.app.Activity;
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.myapplication.R
+
+    class MainActivity extends Activity {
+
+
+
+    }
+}
